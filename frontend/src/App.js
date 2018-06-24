94fd96b7b886a15dea593cb4e2bbf41d2aae5829
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-06-24 20:35:58 -0300

    feat: Apply basic routing for components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import CategoryList from './components/CategoryList';
 import PostList from './components/PostList';
 import { getCategories } from './service/categories-service';
 import { getPosts } from './service/post-service';
+import {Route} from 'react-router-dom';
 
 class App extends Component {
   constructor() {
@@ -32,7 +33,11 @@ class App extends Component {
     return (
       <div className="App">
         <CategoryList categories={this.state.categories}/>
-		<PostList posts={this.state.posts}/>
+		<Route path="/category/:name"
+			render={({match}) => (
+				<PostList posts={this.state.posts} category={match.params.name}/>
+			)}
+		/>
       </div>
     );
   }
--- frontend/src/components/CategoryList.js
+++ frontend/src/components/CategoryList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
 export default class CategoryList extends Component {
 	render() {
@@ -6,7 +7,11 @@ export default class CategoryList extends Component {
 		return (
 			<ul>
 				{categories.map(category => (
-					<li key={category.name}>{category.name}</li>
+					<li key={category.name}>
+						<Link to={`/category/${category.name}`}>
+							{category.name}
+						</Link>
+					</li>
 				))}
 			</ul>
 		);
