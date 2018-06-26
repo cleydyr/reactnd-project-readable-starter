c22a7f3d635470b2f2c2107a1cbf6ab63efdd60c
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-06-26 17:55:42 -0300

    feat: Add human-readable published timestamp to post display

--- frontend/src/components/PostDisplay.js
+++ frontend/src/components/PostDisplay.js
@@ -2,13 +2,12 @@ import React, { Component } from 'react';
 
 export default class PostDisplay extends Component {
 	render() {
-		// console.log(this.props)
-		const {title, voteScore, author, body} = this.props.post;
-		// return <div></div>
+		const {title, voteScore, author, body, timestamp} = this.props.post;
 		return (<div>
 			<h1>{title} ({voteScore})</h1>
 			<em>by {author}</em>
 			<p>{body}</p>
+			<tiny>Published {(new Date(timestamp)).toUTCString()}</tiny>	.
 		</div>);
 	}
 }
