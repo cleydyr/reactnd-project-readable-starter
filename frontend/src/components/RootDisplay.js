9b5616166852f8caff4cb941497a36fb8143d45f
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-06-26 17:51:26 -0300

    feat: Create component RootDisplay

---
+++ frontend/src/components/RootDisplay.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import CategoryList from './CategoryList';
+import PostList from './PostList';
+
+export default function(props) {
+	const {categoryName, categories, posts} = props;
+	return (
+		<React.Fragment>
+			<h3>Categories</h3>
+			<CategoryList categories={categories}/>
+			<h3>Posts</h3>
+			<PostList posts={posts.filter(post => !categoryName || post.category === categoryName)}/>
+		</React.Fragment>
+	);
+}
