c72674bd3a1d5ad3fb0b3a0ed8764b5e672b3fbe
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-06-30 16:59:43 -0300

    refactor: SF

--- frontend/src/components/PostDisplay.js
+++ frontend/src/components/PostDisplay.js
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 export default class PostDisplay extends Component {
 	render() {
 		const {title, voteScore, author, body, timestamp} = this.props.post;
-		return (<div>
-			<h1>{title} ({voteScore})</h1>
-			<em>by {author}</em>
-			<p>{body}</p>
-			<tiny>Published {(new Date(timestamp)).toUTCString()}</tiny>	.
-		</div>);
+		return (
+			<div>
+				<h1>{title} ({voteScore})</h1>
+				<em>by {author}</em>
+				<p>{body}</p>
+				<tiny>Published {(new Date(timestamp)).toUTCString()}</tiny>
+			</div>
+		);
 	}
 }
