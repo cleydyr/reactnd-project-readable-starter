64e554f0750cbc12dfd34b64d6fa4131dce9cc59
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-07-17 20:57:11 -0300

    feat: Extract component and add voting functionality to post item

---
+++ frontend/src/components/PostItem.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import VoteControl from './VoteControl';
+import {connect} from 'react-redux';
+import { downVotePost, upVotePost } from '../actions';
+
+class PostItem extends Component {
+
+	downVotePost = () => {
+		this.props.dispatchDownVotePost(this.props.id);
+	}
+
+	upVotePost = () => {
+		this.props.dispatchUpVotePost(this.props.id);
+	}
+
+	render() {
+		const {id, title, author, timestamp, voteScore, commentCount} = this.props;
+		return (
+			<li key={id}>
+				<Link to={`/post/${id}`}>
+					<h3>{title}</h3>
+				</Link>
+				<div><em>Published {(new Date(timestamp)).toUTCString()} by <strong>{author}</strong></em></div>
+				<div><em>({voteScore} {Math.abs(voteScore) === 1 ? 'vote, ' : 'votes, '}
+					{commentCount} {Math.abs(commentCount) === 1 ? 'comment ' : 'comments '})</em></div>
+				<VoteControl
+						voteScore={voteScore}
+						onDownVote={this.downVotePost}
+						onUpVote={this.upVotePost}
+				/>
+			</li>
+		);
+	}
+}
+
+const mapDispatchToProps = dispatch => ({
+	dispatchDownVotePost: postId => dispatch(downVotePost({postId})),
+	dispatchUpVotePost: postId => dispatch(upVotePost({postId})),
+});
+
+export default connect(null, mapDispatchToProps)(PostItem);
--- frontend/src/components/PostList.js
+++ frontend/src/components/PostList.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import {Link} from "react-router-dom";
 import { connect } from 'react-redux';
+import PostItem from './PostItem';
 
 class PostList extends Component {
 	render() {
@@ -13,17 +13,8 @@ class PostList extends Component {
 		return (
 			<ul>
 				{
-					postsWithCommentCount
-						.map(({id, title, author, timestamp, voteScore, commentCount}) => (
-							<li key={id}>
-								<Link to={`/post/${id}`}>
-									<h3>{title}</h3>
-								</Link>
-								<div><em>Published {(new Date(timestamp)).toUTCString()} by <strong>{author}</strong></em></div>
-								<div><em>({voteScore} {Math.abs(voteScore) === 1 ? 'vote, ' : 'votes, '}
-									{commentCount} {Math.abs(commentCount) === 1 ? 'comment ' : 'comments '})</em></div>
-								<small></small>
-							</li>))}
+					postsWithCommentCount.map(post => <PostItem {...post}/>)
+				}
 			</ul>
 		);
 	}
