0cebf8591014f60db42bb17b9c0c2e0239f4f97e
Cleydyr Albuquerque <cleydyr.albuquerque@liferay.com>
2018-07-19 17:16:11 -0300

    fix: Change to absolute path

--- frontend/src/components/EditDeletePostButton.js
+++ frontend/src/components/EditDeletePostButton.js
@@ -4,11 +4,11 @@ import {Link} from 'react-router-dom';
 export default ({id}) => (
 	<div>
 		<span>
-			<Link to={`edit/${id}`}>Edit</Link>
+			<Link to={`/edit/${id}`}>Edit</Link>
 		</span>
 		&nbsp;
 		<span>
-			<Link to={`delete/${id}`}>Delete</Link>
+			<Link to={`/delete/${id}`}>Delete</Link>
 		</span>
 	</div>
 );
